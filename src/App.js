5a17570fbf92a2a86ca6b2e29db22486a9365656
hcmishra512 <h.c.mishra512@gmail.com>
2021-10-22 22:57:47 +0530

    updated source code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
-import ExpenseItem from "./Components/ExpenseItem";
+import React from "react";
 
-function App() {
+import Expenses from "./Components/Expenses";
+
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -10,60 +10,40 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 4, 12) },
-
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
       title: "Car Insurance",
-      amount: 694.67,
+      amount: 294.67,
       date: new Date(2021, 2, 28),
     },
-
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-
     {
       id: "e5",
       title: "Bike Insurance",
-      amount: 150,
-      date: new Date(2021, 8, 12),
+      amount: 110,
+      date: new Date(2021, 10, 22),
     },
   ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <h1>Let's get started!</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
-      <ExpenseItem
-        title={expenses[4].title}
-        amount={expenses[4].amount}
-        date={expenses[4].date}
-      />
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
